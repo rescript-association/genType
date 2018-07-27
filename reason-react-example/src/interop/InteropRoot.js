16a167f92c3328044aa17f855b184cde834aa4b7
Cristiano Calcagno <ccrisccris@gmail.com>
2018-07-27 10:45:32 +0200

    Don’t require “M.re” but require “M.re.js” instead.
    There’s no semantic difference, except, requiring `M.re` confuses webpack, which finds the reason files, and tries to parse it.

--- reason-react-example/src/interop/InteropRoot.js
+++ reason-react-example/src/interop/InteropRoot.js
@@ -4,9 +4,9 @@ var ReactDOM = require('react-dom');
 var React = require('react');
 
 // Import a ReasonReact component! `jsComponent` is the exposed, underlying ReactJS class
-var PageReason = require('./GreetingRe.re').component;
+var PageReason = require('./GreetingRe.re.js').component;
 
-var App = () => <div> <PageReason message="Hello!"/> </div>;
+var App = () => <div> <PageReason message="This is from genFlow!"/> </div>;
 App.displayName = 'ExampleInteropRoot';
 
 // $FlowFixMe
