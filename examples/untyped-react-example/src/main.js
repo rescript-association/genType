24c1fc3aaec6f27ca722c3181e6a339ddad00463
Cristiano Calcagno <ccrisccris@gmail.com>
2018-10-04 11:07:26 +0200

    Fix untyped-react-example.

--- examples/untyped-react-example/src/main.js
+++ examples/untyped-react-example/src/main.js
@@ -1,7 +1,7 @@
 const ReactDOM = require("react-dom");
 const React = require("react");
 
-const App = require("./App.bs").default;
+const App = require("./App.re").default;
 
 const Main = () => (
   <div>
