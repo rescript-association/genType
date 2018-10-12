154729e3cf5e188cadbb88ee94f3aa85b907169b
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-10-12 16:23:51 +0200

    Remove unnecessary newline

---
+++ scripts/bump_version_module.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const pjson = require("../package.json");
+const path = require("path");
+
+const targetFile = path.join(__dirname, "..", "src", "Version.re");
+
+const content = `
+/* CREATED BY genType/scripts/bump_version_module.js */
+/* DO NOT MODIFY BY HAND, WILL BE AUTOMATICALLY UPDATED BY npm version */
+let version = "${pjson.version}";
+`;
+
+fs.writeFileSync(targetFile, content, "utf8");
