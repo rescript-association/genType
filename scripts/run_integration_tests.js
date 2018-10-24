e4ef336e2039a4e5dd4f22df9012604cf467145c
Patrick Stapfer <ryyppy@users.noreply.github.com>
2018-10-24 11:13:13 +0200

    Add version output test

--- scripts/run_integration_tests.js
+++ scripts/run_integration_tests.js
@@ -10,6 +10,7 @@ const debug = require("debug")("IO");
 const fs = require("fs");
 const child_process = require("child_process");
 const path = require("path");
+const pjson = require("../package.json");
 
 const exampleDirPaths = [
   "reason-react-example",
@@ -17,14 +18,17 @@ const exampleDirPaths = [
   "untyped-react-example"
 ].map(exampleName => path.join(__dirname, "..", "examples", exampleName));
 
-const genTypeFile = path.join(
-  __dirname,
-  "..",
-  "lib",
-  "bs",
-  "native",
-  "gentype.native"
-);
+function getGenTypeFilePath() {
+  const os = process.platform;
+  const base = path.join(__dirname, "..", "lib", "bs", "native");
+  if (/^win/i.test(os)) {
+    return path.join(base, "gentype.native.exe");
+  } else {
+    return path.join(base, "gentype.native");
+  }
+}
+
+const genTypeFile = getGenTypeFilePath();
 
 /*
 Needed for wrapping the stdout pipe with a promise
@@ -79,8 +83,30 @@ async function checkDiff() {
 
 async function checkSetup() {
   if (!fs.existsSync(genTypeFile)) {
-      const filepath = path.relative(path.join(__dirname, ".."), genTypeFile);
-      throw new Error(`${filepath} does not exist. Use \`npm run build\` first!`);
+    const filepath = path.relative(path.join(__dirname, ".."), genTypeFile);
+    throw new Error(`${filepath} does not exist. Use \`npm run build\` first!`);
+  }
+
+  let output;
+  /* Compare the --version output with the package.json version number (should match) */
+  try {
+    output = child_process.execFileSync(genTypeFile, ["--version"], {
+      encoding: "utf8"
+    });
+  } catch (e) {
+    throw new Error(
+      `gentype --version caused an unexpected error: ${e.message}`
+    );
+  }
+
+  if (output !== `genType version ${pjson.version}\n`) {
+    throw new Error(
+      `${path.basename(
+        genTypeFile
+      )} --version doesn't match the version number of package.json  (${output} != ${
+        pjson.version
+      }) - Run \`node scripts/bump_version_module.js\` and rebuild to sync version numbers`
+    );
   }
 }
 
