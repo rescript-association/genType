82be1839687b73e0feb9f527cc83c9c5219bcb5d
Cristiano Calcagno <ccrisccris@gmail.com>
2019-01-05 07:28:18 +0100

    Use the correct variant payloads for variants as Enums.

--- examples/flow-react-example/src/Variants.gen.js
+++ examples/flow-react-example/src/Variants.gen.js
@@ -4,15 +4,15 @@
  * @nolint
  */
 
-const YY = {"NoOp": 0, "AdjustAge": 1, "Booly": 2, "OptionalInt": 3, "Unity": 4, "OptionalBooly": 5, "OptionalBoolMapper": 6};
+const YY = {"NoOp": 0};
 
 // $FlowExpectedError: Reason checked type sufficiently
 import * as VariantsBS from './Variants.bs';
 
-export type action = "NoOp" | "AdjustAge" | "Booly" | "OptionalInt" | "Unity" | "OptionalBooly" | "OptionalBoolMapper";
+export type action = "NoOp" | {|tag: "AdjustAge", value: (number) => number|} | {|tag: "Booly", value: boolean|} | {|tag: "OptionalInt", value: ?number|} | {|tag: "Unity", value: void|} | {|tag: "OptionalBooly", value: ?boolean|} | {|tag: "OptionalBoolMapper", value: (?boolean) => ?boolean|};
 
 export type optionalBoolMapper = {|+optionalBoolMapper: (?boolean) => ?boolean|};
 
-export const actionToString: (action) => string = function _(Arg1) { const result = VariantsBS.actionToString(YY[Arg1]); return result };
+export const actionToString: (action) => string = function _(Arg1) { const result = VariantsBS.actionToString((typeof(Arg1) === 'object' ? (Arg1.tag==="AdjustAge" ? [/* 0 */48, Arg1.value] :  Arg1.tag==="Booly" ? [/* 1 */49, Arg1.value] :  Arg1.tag==="OptionalInt" ? [/* 2 */50, (Arg1.value == null ? undefined : Arg1.value)] :  Arg1.tag==="Unity" ? [/* 3 */51, Arg1.value] :  Arg1.tag==="OptionalBooly" ? [/* 4 */52, (Arg1.value == null ? undefined : Arg1.value)] :  [/* 5 */53, function _(Arg11) { const result1 = Arg1.value(Arg11); return (result1 == null ? undefined : result1) }]) : YY[Arg1])); return result };
 
 export const converter: (optionalBoolMapper) => optionalBoolMapper = function _(Arg1) { const result = VariantsBS.converter([function _(Arg11) { const result1 = Arg1.optionalBoolMapper(Arg11); return (result1 == null ? undefined : result1) }]); return {optionalBoolMapper:function _(Arg12) { const result2 = result[0]((Arg12 == null ? undefined : Arg12)); return result2 }} };
--- examples/typescript-react-example/src/EnumsWithPayload.gen.tsx
+++ examples/typescript-react-example/src/EnumsWithPayload.gen.tsx
@@ -2,7 +2,7 @@
 
 const $$toJS542320962 = {"97": "a", "98": "bRenamed", "937218926": true, "-574635695": 20, "803296723": 0.5};
 
-const XX = {"0": "A", "1": "B", "2": "C"};
+const XX = {"0": "C"};
 
 const $$toRE1058613066 = {};
 
@@ -10,7 +10,7 @@ const $$toJS1058613066 = {};
 
 const $$toRE542320962 = {"a": 97, "bRenamed": 98, "true": 937218926, "20": -574635695, "0.5": 803296723};
 
-const YY = {"A": 0, "B": 1, "C": 2};
+const YY = {"C": 0};
 
 // tslint:disable-next-line:no-var-requires
 const EnumsWithPayloadBS = require('./EnumsWithPayload.bs');
@@ -25,7 +25,7 @@ export type withPayload = "a" | "bRenamed" | true | 20 | 0.5 | payload;
 export type manyPayloads = {tag: "one", value: number} | {tag: "two", value: [string, string]} | {tag: "three", value: payload};
 
 // tslint:disable-next-line:interface-over-type-literal
-export type simpleVariant = "A" | "B" | "C";
+export type simpleVariantWithPayloads = "C" | {tag: "A", value: [number, number]} | {tag: "B", value: [number, number]};
 
 export const testWithPayload: (_1:withPayload) => withPayload = function _(Arg1: any) { const result = EnumsWithPayloadBS.testWithPayload((typeof(Arg1) === 'object' ? [/* c */99, [Arg1.x, Arg1.y]] : $$toRE542320962[Arg1.toString()])); return (typeof(result) === 'object' ? {x:result[1][0], y:result[1][1]} : $$toJS542320962[result]) };
 
@@ -35,4 +35,4 @@ export const testManyPayloads: (_1:manyPayloads) => manyPayloads = function _(Ar
 
 export const printManyPayloads: (_1:manyPayloads) => void = function _(Arg1: any) { const result = EnumsWithPayloadBS.printManyPayloads((typeof(Arg1) === 'object' ? (Arg1.tag==="one" ? [/* one */5544550, Arg1.value] :  Arg1.tag==="two" ? [/* two */5795212, Arg1.value] :  [/* three */261117022, [Arg1.value.x, Arg1.value.y]]) : $$toRE1058613066[Arg1])); return result };
 
-export const testSimpleVariant: (_1:simpleVariant) => simpleVariant = function _(Arg1: any) { const result = EnumsWithPayloadBS.testSimpleVariant(YY[Arg1]); return XX[result] };
+export const testSimpleVariantWithPayloads: (_1:simpleVariantWithPayloads) => simpleVariantWithPayloads = function _(Arg1: any) { const result = EnumsWithPayloadBS.testSimpleVariantWithPayloads((typeof(Arg1) === 'object' ? (Arg1.tag==="A" ? [/* 0 */48, Arg1.value] :  [/* 1 */49, Arg1.value]) : YY[Arg1])); return (typeof(result) === 'object' ? (result[0]===/* 0 */48 ? {tag:"A", value:result[1]} :  {tag:"B", value:result[1]}) : XX[result]) };
