791f96283fc796bb63cfe2c9baa1211d15d7a5d9
Logan Call <logancall.dev@gmail.com>
2019-02-07 16:00:06 -0700

    Add ReactShim and update bsconfig in typescript example. https://github.com/cristianoc/genType/issues/135#issuecomment-461609145

---
+++ examples/typescript-react-example/src/shims/ReactShim.shim.ts
@@ -0,0 +1,28 @@
+// tslint:disable-next-line:max-classes-per-file
+export abstract class reactElement {
+  protected opaque: unknown
+}
+// tslint:disable-next-line:max-classes-per-file
+export abstract class component {
+  protected opaque: unknown
+}
+// tslint:disable-next-line:max-classes-per-file
+export abstract class componentSpec {
+  protected opaque: unknown
+}
+// tslint:disable-next-line:max-classes-per-file
+export abstract class noRetainedProps {
+  protected opaque: unknown
+}
+// tslint:disable-next-line:max-classes-per-file
+export abstract class actionless {
+  protected opaque: unknown
+}
+// tslint:disable-next-line:max-classes-per-file
+export abstract class stateless {
+  protected opaque: unknown
+}
+// tslint:disable-next-line:max-classes-per-file
+export abstract class reactRef {
+  protected opaque: unknown
+}
