0a90b2397488250fa253e28ee6b620f977399816
Cristiano Calcagno <ccrisccris@gmail.com>
2019-03-05 12:45:13 +0100

    Add example of import requiring variant conversion.

--- examples/typescript-react-example/src/MyMath.ts
+++ examples/typescript-react-example/src/MyMath.ts
@@ -26,3 +26,5 @@ export const useColor = function(x: "tomato" | "gray"): number {
 
 
 export const higherOrder = (foo: (_1:number, _2:number) => number) => foo(3,4);
+
+export const convertVariant = (x:any) => x;
