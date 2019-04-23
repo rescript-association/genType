b434232fb7d0080f625f89b6a7ece38411d4de03
Cristiano Calcagno <ccrisccris@gmail.com>
2019-04-23 10:02:23 +0200

    Extend Opaque example.

--- examples/typescript-react-example/src/Opaque.gen.tsx
+++ examples/typescript-react-example/src/Opaque.gen.tsx
@@ -7,4 +7,8 @@ const OpaqueBS = require('./Opaque.bs');
 // tslint:disable-next-line:class-name
 export abstract class opaqueFromRecords { protected opaque!: any }; /* simulate opaque types */
 
+// tslint:disable-next-line:max-classes-per-file 
+// tslint:disable-next-line:class-name
+export abstract class pair { protected opaque!: any }; /* simulate opaque types */
+
 export const noConversion: (_1:opaqueFromRecords) => opaqueFromRecords = OpaqueBS.noConversion;
