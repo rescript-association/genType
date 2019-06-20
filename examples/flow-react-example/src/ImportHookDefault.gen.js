c70f13ecb6acad3b40f7e8b0e44afc504f09612f
Cristiano Calcagno <ccrisccris@gmail.com>
2019-06-20 18:42:46 +0200

    Use the same example with render props in Flow.

--- examples/flow-react-example/src/ImportHookDefault.gen.js
+++ examples/flow-react-example/src/ImportHookDefault.gen.js
@@ -11,11 +11,17 @@ type $any = any;
 import {default as makeNotChecked} from './hookExample';
 
 // In case of type error, check the type of 'make' in 'ImportHookDefault.re' and './hookExample'.
-export const makeTypeChecked: ({| +person: person, +children: React$Node |}) => React$Node = makeNotChecked;
+export const makeTypeChecked: ({|
+  +person: person, 
+  +children: React$Node, 
+  +renderMe: (({|
+    +randomString: string
+  |}) => React$Node)
+|}) => React$Node = makeNotChecked;
 
 // Export 'make' early to allow circular import from the '.bs.js' file.
 export const make: mixed = function hookExample(Arg1: $any) {
-  const result = makeTypeChecked({person:{name:Arg1.person[0], age:Arg1.person[1]}, children:Arg1.children});
+  const result = makeTypeChecked({person:{name:Arg1.person[0], age:Arg1.person[1]}, children:Arg1.children, renderMe:Arg1.renderMe});
   return result
 };
 
