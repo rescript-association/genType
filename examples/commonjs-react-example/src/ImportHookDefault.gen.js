b8fa76702372321f98c7a0b1d1e5184e21dcd3a9
Cristiano Calcagno <ccrisccris@gmail.com>
2019-08-15 13:56:02 +0200

    Fix issue where two constants with the same name would be generated in CommonJS mode.
    Fixes https://github.com/cristianoc/genType/issues/232.

--- examples/commonjs-react-example/src/ImportHookDefault.gen.js
+++ examples/commonjs-react-example/src/ImportHookDefault.gen.js
@@ -6,10 +6,10 @@
 /* eslint-disable */
 
 // flowlint-next-line nonstrict-import:off
-const hookExample = require('./hookExample');
+const $$hookExample = require('./hookExample');
 
 // In case of type error, check the type of 'make' in 'ImportHookDefault.re' and './hookExample'.
-const makeTypeChecked: React$ComponentType<{| +show: boolean, +Message?: string |}> = hookExample;;
+const makeTypeChecked: React$ComponentType<{| +show: boolean, +Message?: string |}> = $$hookExample;;
 exports.makeTypeChecked = makeTypeChecked
 
 // Export 'make' early to allow circular import from the '.bs.js' file.
@@ -17,7 +17,7 @@ const make: mixed = makeTypeChecked;;
 exports.make = make
 
 // In case of type error, check the type of 'default' in 'ImportHookDefault.re' and './hookExample'.
-const defaultTypeChecked: React$ComponentType<{| +show: boolean, +Message?: string |}> = hookExample;;
+const defaultTypeChecked: React$ComponentType<{| +show: boolean, +Message?: string |}> = $$hookExample;;
 exports.defaultTypeChecked = defaultTypeChecked
 
 // Export '$$default' early to allow circular import from the '.bs.js' file.
