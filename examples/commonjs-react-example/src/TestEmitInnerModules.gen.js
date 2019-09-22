f9165cf7e9c2202859c0c77e1af1b525af7e702e
Cristiano Calcagno <ccrisccris@gmail.com>
2019-09-22 16:48:59 +0200

    Added untyped and commonjs example.
    For commonjs, the export should use the differenct convention.

---
+++ examples/commonjs-react-example/src/TestEmitInnerModules.gen.js
@@ -0,0 +1,22 @@
+/** 
+ * @flow strict
+ * @generated
+ * @nolint
+ */
+/* eslint-disable */
+
+// $FlowExpectedError: Reason checked type sufficiently
+const TestEmitInnerModulesBS = require('./TestEmitInnerModules.bs');
+
+const Inner_x: number = TestEmitInnerModulesBS.Inner[0];;
+exports.Inner_x = Inner_x
+
+const Inner_y: string = TestEmitInnerModulesBS.Inner[1];;
+exports.Inner_y = Inner_y
+
+const Outer_Medium_Inner_y: number = TestEmitInnerModulesBS.Outer[0][0][0];;
+exports.Outer_Medium_Inner_y = Outer_Medium_Inner_y
+
+export const Outer = { Medium: { Inner: { y: Outer_Medium_Inner_y } } };
+
+export const Inner = { y: Inner_y, x: Inner_x };
