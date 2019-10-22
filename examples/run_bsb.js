e96fb9f7f9fd17202e861c98cd04e8c8abba0cd5
Patrick Stapfer <ryyppy@users.noreply.github.com>
2019-10-22 12:02:00 +0200

    Rename gentype.native.exe to gentype.exe

--- examples/run_bsb.js
+++ examples/run_bsb.js
@@ -10,19 +10,9 @@ const spawn = child_process.spawn;
 
 const isWindows = /^win/i.test(process.platform);
 
-function copyNativeToExe() {
-  const base = path.join(__dirname, "..", "src", "lib", "bs", "native");
-  if (!isWindows) {
-    fs.copyFileSync(path.join(base, "gentype.native"), path.join(base, "gentype.native.exe"));
-  }
-}
-
 const input = (args = process.argv.slice(2));
 
 const shell = isWindows ? true : false;
-
-// Not needed, since dune builds gentype.native.exe files by default
-//copyNativeToExe();
 
 spawn("bsb", input, { stdio: ["inherit", "inherit"], shell }).on(
   "exit",
