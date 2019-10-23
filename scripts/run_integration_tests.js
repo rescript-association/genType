55b3c1f9b9dc42faf68a1d964a31aec759e8cd3f
Patrick Stapfer <ryyppy@users.noreply.github.com>
2019-10-23 10:54:16 +0200

    forgot the ls command...

--- scripts/run_integration_tests.js
+++ scripts/run_integration_tests.js
@@ -110,7 +110,7 @@ function checkSetup() {
     throw new Error("This script cannot be run with `esy`. Use `npm test` instead!");
   }
 
-  const lsOutput = child_process.execFileSync(path.resolve(__dirname, ".."));
+  const lsOutput = child_process.execFileSync("ls", ['-l', path.resolve(__dirname, "..")]);
   console.log(lsOutput);
 
   console.log(`Check existing binary: ${genTypeFile}`);
