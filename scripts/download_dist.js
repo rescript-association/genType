0ef2cdafc8a43887711b1ce1d49f7d8610716c43
Patrick Stapfer <ryyppy@users.noreply.github.com>
2019-10-24 20:19:14 +0200

    Cleanup readmes for master (#281)
    * Change prepare_dist to download_dist script
    
    Since npm packages are prepacked on Azure, we only
    need to download the released package from Github and
    publish it via `npm`.
    
    This change adapts to this workflow.
    
    * Have correct CI badges for master + 4_02
    
    * Ignore path for downloaded npm packages
    
    * Adapt to new DEVELOPMENT
    
    * Remove pro-tip, since this is not possible anymore
    
    * Format status badges better
    
    * Feedback changes

---
+++ scripts/download_dist.js
@@ -0,0 +1,35 @@
+/*
+PLEASE NOTE:
+This script is intended to be used on unix-like systems and used for publishing npm packages.
+*/
+
+const child_process = require("child_process");
+const pjson = require("../package.json");
+const path = require("path");
+
+const projectRoot = path.join(__dirname, "..");
+
+const filename = `gentype-${pjson.version}.tgz`;
+
+const targetLink = (version) =>
+  `https://github.com/cristianoc/genType/releases/download/v${version}/${filename}`;
+
+const target = targetLink(pjson.version);
+
+console.log(`Download release: "${target}" ...`);
+try {
+  child_process.execSync(
+    `curl -f -O -L ${target}`,
+    { stdio: [0, 1, 2] }
+  );
+  console.log(`File downloaded to ${projectRoot}/${filename}`);
+  console.log("------------");
+  console.log(`Run \`npm publish ${filename} --dry-run\` before release!`);
+  console.log("------------");
+} catch (e) {
+  console.error(
+    `Download failed... Is the package ${filename} actually released?`
+  );
+  console.error(e);
+  process.exit(1);
+}
