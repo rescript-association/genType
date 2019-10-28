759386d12187d23c0acc2d9590977503d523cafe
Cristiano Calcagno <ccrisccris@gmail.com>
2019-10-28 08:09:11 +0100

    Remove remaining cppo artefacts.

--- src/ext/string_map.ml
+++ src/ext/string_map.ml
@@ -1,14 +1,11 @@
 
-# 2 "ext/map.cppo.ml"
 (* we don't create [map_poly], since some operations require raise an exception which carries [key] *)
 
 
   
-# 10
   type key = string 
   let compare_key = Ext_string.compare
 
-# 22
 type 'a t = (key,'a) Map_gen.t
 exception Duplicate_key of key 
 
