7add0523f6b38d327a74a424531d3a5b8269b501
Cristiano Calcagno <ccrisccris@gmail.com>
2019-11-06 13:31:02 +0100

    Add support for `[@bs.meth]`.

---
+++ examples/flow-react-example/src/BucklescriptAnnotations.gen.js
@@ -0,0 +1,19 @@
+/** 
+ * @flow strict
+ * @generated
+ * @nolint
+ */
+/* eslint-disable */
+
+export type someMutableFields = {|
+  mutable0: string, 
+  +immutable: number, 
+  mutable1: string, 
+  mutable2: string
+|};
+
+export type someMethods = {|
+  +send: (string) => void, 
+  +on: ([string, ((number) => void)]) => void, 
+  +treeargs: ([number, string, number]) => string
+|};
