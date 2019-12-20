5f269575ec9ff2d8a9f18aad1001218b38825216
Cristiano Calcagno <ccrisccris@gmail.com>
2019-12-20 06:27:45 +0100

    Use bucklescript 7.0.2-dev.1.

--- examples/typescript-react-example/src/Unboxed.gen.tsx
+++ examples/typescript-react-example/src/Unboxed.gen.tsx
@@ -3,29 +3,20 @@
 
 
 // tslint:disable-next-line:no-var-requires
-const CreateBucklescriptBlock = require('bs-platform/lib/es6/block.js');
-
-// tslint:disable-next-line:no-var-requires
 const UnboxedBS = require('./Unboxed.bs');
 
 // tslint:disable-next-line:interface-over-type-literal
-export type v1 = { tag: "A"; value: number };
+export type v1 = number;
 
 // tslint:disable-next-line:interface-over-type-literal
-export type v2 = { tag: "A"; value: number };
+export type v2 = number;
 
 // tslint:disable-next-line:interface-over-type-literal
-export type r1 = { readonly x: number };
+export type r1 = number;
 
 // tslint:disable-next-line:interface-over-type-literal
-export type r2 = { readonly g: string };
+export type r2 = string;
 
-export const testV1: (_1:v1) => v1 = function (Arg1: any) {
-  const result = UnboxedBS.testV1(CreateBucklescriptBlock.__(0, [Arg1.value]));
-  return {tag:"A", value:result[0]}
-};
+export const testV1: (_1:v1) => v1 = UnboxedBS.testV1;
 
-export const r2Test: (_1:r2) => r2 = function (Arg1: any) {
-  const result = UnboxedBS.r2Test(CreateBucklescriptBlock.__(0, Arg1));
-  return result
-};
+export const r2Test: (_1:r2) => r2 = UnboxedBS.r2Test;
