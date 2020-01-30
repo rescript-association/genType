f768825c7cb679f936511a09ae8355c0cd44b268
Cristiano Calcagno <ccrisccris@gmail.com>
2020-01-30 13:53:35 +0100

    flow examples

--- examples/flow-react-example/src/components/Navigator.gen.js
+++ examples/flow-react-example/src/components/Navigator.gen.js
@@ -16,14 +16,14 @@ import * as ReasonReact from 'reason-react/src/ReasonReact.js';
 
 export type Props = {|
   +history: mixed, 
-  +match_: mixed, 
+  +match: mixed, 
   +children?: mixed
 |};
 
 export const component: React$ComponentType<Props> = ReasonReact.wrapReasonForJs(
   NavigatorBS.component,
   (function _(jsProps: Props) {
-     return Curry._3(NavigatorBS.make, jsProps.history, jsProps.match_, jsProps.children);
+     return Curry._3(NavigatorBS.make, jsProps.history, jsProps.match, jsProps.children);
   }));
 
 export default component;
