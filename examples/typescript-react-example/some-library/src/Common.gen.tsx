63d603ae6acb8050a7e5c85ecdc32895e6e7bbd9
Cristiano Calcagno <ccrisccris@gmail.com>
2020-03-10 18:14:06 +0100

    Update TS examples to bs 7.2.1.

--- examples/typescript-react-example/some-library/src/Common.gen.tsx
+++ examples/typescript-react-example/some-library/src/Common.gen.tsx
@@ -8,7 +8,4 @@ const CommonBS = require('./Common.bs');
 // tslint:disable-next-line:interface-over-type-literal
 export type t = { readonly lib: string };
 
-export const foo: () => t = function () {
-  const result = CommonBS.foo();
-  return {lib:result[0]}
-};
+export const foo: () => t = CommonBS.foo;
