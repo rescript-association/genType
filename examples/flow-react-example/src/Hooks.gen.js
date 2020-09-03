9a36b9bf9e5fbbb67b3deb8ecd7acf698effbd32
Cristiano Calcagno <ccrisccris@gmail.com>
2020-09-03 09:30:57 +0200

    Make `React.ref(-)` types open objects, to align with how they are modeled by Flow.

--- examples/flow-react-example/src/Hooks.gen.js
+++ examples/flow-react-example/src/Hooks.gen.js
@@ -26,11 +26,11 @@ export type callback<input,output> = (input) => output;
 
 export type testReactContext = React$Context<number>;
 
-export type testReactRef = {| current: (null | number) |};
+export type testReactRef = { current: (null | number), ... };
 
 export type testDomRef = React$Ref<mixed>;
 
-export type notAFunctionComponent = ({| current: (null | number) |}) => React$Node;
+export type notAFunctionComponent = ({ current: (null | number), ... }) => React$Node;
 
 // Type annotated function components are not checked by Flow, but typeof() works.
 const $$default$$forTypeof = function (_: {| +vehicle: vehicle |}) : React$Node { return null };
