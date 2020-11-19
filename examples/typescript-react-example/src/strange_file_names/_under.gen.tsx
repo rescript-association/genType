250aab90a3d098eec715e78fcace7e9d19751dde
Cristiano Calcagno <ccrisccris@gmail.com>
2020-11-19 12:29:41 +0100

    Add support for flexible file names introduced in bs-platform 8.3.
    Fixes https://github.com/reason-association/genType/issues/477

---
+++ examples/typescript-react-example/src/strange_file_names/_under.gen.tsx
@@ -0,0 +1,8 @@
+/* TypeScript file generated from _under.res by genType. */
+/* eslint-disable import/first */
+
+
+// tslint:disable-next-line:no-var-requires
+const _underBS = require('./_under.bs');
+
+export const x: number = _underBS.x;
---
+++ examples/typescript-react-example/src/strange_file_names/with_underscore.gen.tsx
@@ -0,0 +1,8 @@
+/* TypeScript file generated from with_underscore.res by genType. */
+/* eslint-disable import/first */
+
+
+// tslint:disable-next-line:no-var-requires
+const with_underscoreBS = require('./with_underscore.bs');
+
+export const x: number = with_underscoreBS.x;
