07786e9dcf637cabd10001297697417b6ce1bb6e
Cristiano Calcagno <ccrisccris@gmail.com>
2021-04-13 21:56:32 +0200

    Fix issue where `let x:t = ...` is treated differently in `.res` syntax.

---
+++ examples/flow-react-example/src/LetWithType.gen.js
@@ -0,0 +1,13 @@
+/** 
+ * @flow strict
+ * @generated from LetWithType.res
+ * @nolint
+ */
+/* eslint-disable */
+
+// $FlowExpectedError: Reason checked type sufficiently
+import * as LetWithTypeBS from './LetWithType.bs';
+
+export opaque type style = mixed;
+
+export const white: style = LetWithTypeBS.white;
