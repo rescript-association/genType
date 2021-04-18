38bba8b1cd9502595ee44ed0868dc06c4e985887
Cristiano Calcagno <ccrisccris@gmail.com>
2021-04-18 09:49:45 +0200

    Convert ImportPath ExportModule.

---
+++ src/ImportPath.ml
@@ -0,0 +1,36 @@
+open GenTypeCommon
+
+type t = string * string
+
+let propTypes = ("", "prop-types")
+
+let react = ("", "react")
+
+let reasonReactPath ~config = ("", config.reasonReactPath)
+
+let bsCurryPath ~config = ("", Config_.getBsCurryPath ~config)
+
+let fromModule ~dir ~importExtension moduleName =
+  let withNoPath =
+    (moduleName |> ModuleName.toString |> ScopedPackage.removeGeneratedModule)
+    ^ importExtension
+  in
+  (dir, withNoPath)
+
+let fromStringUnsafe s = ("", s)
+
+let chopExtensionSafe s =
+  try s |> Filename.chop_extension with Invalid_argument _ -> s
+
+let dump (dir, s) = NodeFilename.concat dir s
+
+let toCmt ~config ~outputFileRelative (dir, s) =
+  let open Filename in
+  concat
+    (outputFileRelative |> dirname)
+    (((dir, s |> chopExtensionSafe) |> dump)
+    ^ (match config.namespace with None -> "" | Some name -> "-" ^ name)
+    ^ ".cmt")
+
+let emit ~config (dir, s) =
+  match config.importPath with Relative -> (dir, s) |> dump | Node -> s
