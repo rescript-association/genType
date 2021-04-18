3b883fbc914b2c17ee87350c4e4caa48bf27f134
Cristiano Calcagno <ccrisccris@gmail.com>
2021-04-18 08:12:43 +0200

    Convert Gen*.

---
+++ src/GenTypeCommon.ml
@@ -0,0 +1,250 @@
+module StringMap = Map.Make (String)
+module StringSet = Set.Make (String)
+module Config = Config_
+include Config
+
+let logNotImplemented x =
+  if !Debug.notImplemented then Log_.item "Not Implemented: %s\n" x
+
+type optional = Mandatory | Optional
+
+type mutable_ = Immutable | Mutable
+
+type labelJS =
+  | BoolLabel of bool
+  | FloatLabel of string
+  | IntLabel of string
+  | StringLabel of string
+
+type case = {label : string; labelJS : labelJS}
+
+let labelJSToString ?(alwaysQuotes = false) case =
+  let addQuotes x =
+    match alwaysQuotes with true -> x |> EmitText.quotes | false -> x
+  in
+  let isNumber s =
+    let len = String.length s in
+    len > 0
+    && (match len > 1 with true -> s.[0] > '0' | false -> true)
+    &&
+    let res = ref true in
+    for i = 0 to len - 1 do
+      match s.[i] with '0' .. '9' -> () | _ -> res := false
+    done;
+    res.contents
+  in
+  match case.labelJS with
+  | ((BoolLabel b)[@explicit_arity]) -> b |> string_of_bool |> addQuotes
+  | ((FloatLabel s)[@explicit_arity]) -> s |> addQuotes
+  | ((IntLabel i)[@explicit_arity]) -> i |> addQuotes
+  | ((StringLabel s)[@explicit_arity]) ->
+    if s = case.label && isNumber s then s |> addQuotes
+    else s |> EmitText.quotes
+
+type closedFlag = Open | Closed
+
+type type_ =
+  | Array of type_ * mutable_
+  | Function of function_
+  | GroupOfLabeledArgs of fields
+  | Ident of ident
+  | Null of type_
+  | Nullable of type_
+  | Object of closedFlag * fields
+  | Option of type_
+  | Promise of type_
+  | Record of fields
+  | Tuple of type_ list
+  | TypeVar of string
+  | Variant of variant
+
+and fields = field list
+
+and argType = {aName : string; aType : type_}
+
+and field = {
+  mutable_ : mutable_;
+  nameJS : string;
+  nameRE : string;
+  optional : optional;
+  type_ : type_;
+}
+
+and function_ = {
+  argTypes : argType list;
+  componentName : string option;
+  retType : type_;
+  typeVars : string list;
+  uncurried : bool;
+}
+
+and ident = {builtin : bool; name : string; typeArgs : type_ list}
+
+and variant = {
+  bsStringOrInt : bool;
+  hash : int;
+  noPayloads : case list;
+  payloads : payload list;
+  polymorphic : bool;
+  unboxed : bool;
+}
+
+and payload = {case : case; inlineRecord : bool; numArgs : int; t : type_}
+
+let typeIsObject type_ =
+  match type_ with
+  | Array _ -> true
+  | Function _ -> false
+  | GroupOfLabeledArgs _ -> false
+  | Ident _ -> false
+  | Null _ -> false
+  | Nullable _ -> false
+  | Object _ -> true
+  | Option _ -> false
+  | Promise _ -> true
+  | Record _ -> true
+  | Tuple _ -> true
+  | TypeVar _ -> false
+  | Variant _ -> false
+
+type label = Nolabel | Label of string | OptLabel of string
+
+type dep =
+  | External of string
+  | Internal of ResolvedName.t
+  | Dot of dep * string
+
+module ScopedPackage = (* Taken from ext_namespace.ml in bukclescript *)
+struct
+  let namespace_of_package_name (s : string) : string =
+    let len = String.length s in
+    let buf = Buffer.create len in
+    let add capital ch =
+      Buffer.add_char buf (if capital then Char.uppercase_ascii ch else ch)
+    in
+    let rec aux capital off len =
+      if off >= len then ()
+      else
+        let ch = String.unsafe_get s off in
+        match ch with
+        | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' ->
+          add capital ch;
+          aux false (off + 1) len
+        | '/' | '-' -> aux true (off + 1) len
+        | _ -> aux capital (off + 1) len
+    in
+    aux true 0 len;
+    Buffer.contents buf
+
+  (** @demo/some-library -> DemoSomelibrary *)
+  let packageNameToGeneratedModuleName packageName =
+    if String.contains packageName '/' then
+      Some (packageName |> namespace_of_package_name) [@explicit_arity]
+    else None
+
+  let isGeneratedModule id ~config =
+    config.bsDependencies
+    |> List.exists (fun packageName ->
+           packageName |> packageNameToGeneratedModuleName
+           = (Some (id |> Ident.name) [@explicit_arity]))
+
+  (** (Common, DemoSomelibrary) -> Common-DemoSomelibrary *)
+  let addGeneratedModule s ~generatedModule =
+    s ^ "-" ^ Ident.name generatedModule
+
+  (** Common-DemoSomelibrary -> Common *)
+  let removeGeneratedModule s =
+    match s |> String.split_on_char '-' with [name; _scope] -> name | _ -> s
+end
+
+let rec depToString dep =
+  match dep with
+  | ((External name)[@explicit_arity]) ->
+    name |> ScopedPackage.removeGeneratedModule
+  | ((Internal resolvedName)[@explicit_arity]) ->
+    resolvedName |> ResolvedName.toString
+  | ((Dot (d, s))[@explicit_arity]) -> depToString d ^ "_" ^ s
+
+let rec depToResolvedName (dep : dep) =
+  match dep with
+  | ((External name)[@explicit_arity]) -> name |> ResolvedName.fromString
+  | ((Internal resolvedName)[@explicit_arity]) -> resolvedName
+  | ((Dot (p, s))[@explicit_arity]) ->
+    ResolvedName.dot s (p |> depToResolvedName)
+
+let createVariant ~bsStringOrInt ~noPayloads ~payloads ~polymorphic =
+  let hash =
+    noPayloads
+    |> List.map (fun case -> (case.label, case.labelJS))
+    |> Array.of_list |> Hashtbl.hash
+  in
+  let unboxed = payloads = [] in
+  (Variant {bsStringOrInt; hash; noPayloads; payloads; polymorphic; unboxed}
+  [@explicit_arity])
+
+let variantTable hash ~toJS =
+  (match toJS with true -> "$$toJS" | false -> "$$toRE") ^ string_of_int hash
+
+let ident ?(builtin = true) ?(typeArgs = []) name =
+  (Ident {builtin; name; typeArgs} [@explicit_arity])
+
+let sanitizeTypeName name = name |> Str.global_replace (Str.regexp "'") "_"
+
+let mixedOrUnknown ~config =
+  ident
+    (match config.language with
+    | Flow -> "mixed"
+    | TypeScript | Untyped -> "unknown")
+
+let booleanT = ident "boolean"
+
+let dateT = ident "Date"
+
+let numberT = ident "number"
+
+let stringT = ident "string"
+
+let unitT = ident "void"
+
+let int64T = (Tuple [numberT; numberT] [@explicit_arity])
+
+module NodeFilename = struct
+  include Filename
+
+  (* Force "/" separator. *)
+  let dirSep = "/"
+
+  module Path : sig
+    type t
+
+    val normalize : string -> t
+
+    val concat : t -> string -> t
+
+    val toString : t -> string
+  end = struct
+    type t = string
+
+    let normalize path : t =
+      match Sys.os_type with
+      | "Win32" -> path |> Str.split (Str.regexp "\\") |> String.concat dirSep
+      | _ -> path
+
+    let toString path = path
+
+    let length path = String.length path
+
+    let concat dirname filename =
+      let isDirSep s i =
+        let c = s.[i] in
+        c = '/' || c = '\\' || c = ':'
+      in
+      let l = length dirname in
+      if l = 0 || isDirSep dirname (l - 1) then dirname ^ filename
+      else dirname ^ dirSep ^ filename
+  end
+
+  let concat (dirname : string) filename =
+    let open Path in
+    Path.concat (normalize dirname) filename |> toString
+end
