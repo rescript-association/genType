76f49ee2590081a6d79564925abac08cb60493ba
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2021-05-12 10:59:36 +0200

    Add example with onClick.

---
+++ examples/typescript-react-example/src/OnClick2.gen.tsx
@@ -0,0 +1,16 @@
+/* TypeScript file generated from OnClick2.res by genType. */
+/* eslint-disable import/first */
+
+
+import * as React from 'react';
+
+// @ts-ignore: Implicit any on import
+import * as OnClick2BS__Es6Import from './OnClick2.bs';
+const OnClick2BS: any = OnClick2BS__Es6Import;
+
+import {Mouse_t as ReactEvent_Mouse_t} from '../src/shims/ReactEvent.shim';
+
+// tslint:disable-next-line:interface-over-type-literal
+export type Props = { readonly onClick: (_1:ReactEvent_Mouse_t) => void };
+
+export const make: React.ComponentType<{ readonly onClick: (_1:ReactEvent_Mouse_t) => void }> = OnClick2BS.make;
