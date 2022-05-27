359bd541671282ef5c737ad804aa5d83f938c4dd
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-05-27 06:03:15 +0200

    flow

--- examples/flow-react-example/src/shims/ReasonPervasives.shim.js
+++ examples/flow-react-example/src/shims/ReasonPervasives.shim.js
@@ -6,26 +6,8 @@
 export opaque type SafeReasonValueNotExportedToFlow = mixed;
 'use strict';
 
-// $FlowExpectedError[cannot-resolve-module]: Reason checked type sufficiently
-const $$Array = require('bs-platform/lib/js/array');
-
 export opaque type EmptyList = mixed;
 
 export opaque type Cons<t> = mixed;
 
-export type list<t> = | Cons<t> | EmptyList;
 
-function cons<t>(itm, lst) {
-  return /* :: */[
-          itm,
-          lst
-        ];
-}
-
-const emptyList = /* [] */0;
-
-const fromArray = $$Array.to_list;
-
-exports.emptyList = (emptyList : EmptyList);
-exports.cons = (cons : <t>(t, list<t>)=>list<t>);
-exports.fromArray = (fromArray : <t>(Array<t>)=>list<t>);
