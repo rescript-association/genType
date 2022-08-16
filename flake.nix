97aa5ccf38f2f43bf967300642980a65e77d8e74
Quinn Dougherty <quinnd@riseup.net>
2022-08-15 22:42:45 -0700

    updated flake to build on all systems

--- flake.nix
+++ flake.nix
@@ -3,51 +3,52 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        system = system;
-        overlays = [];
-      };
-      gentype-source = pkgs.mkYarnPackage {
-        name = "gentype_source";
-        src = ./.;
-        packageJSON = ./package.json;
-        yarnLock = ./yarn.lock;
-        pkgConfig = {
-          rescript = {
-            buildInputs = with pkgs; [ which gcc_multi ];
-            postInstall = ''
-              echo "PATCHELF'ING RESCRIPT EXECUTABLES (INCL NINJA)"
-              # Patching interpreter for linux/*.exe's
-              THE_LD=$(patchelf --print-interpreter $(which mkdir))
-              patchelf --set-interpreter $THE_LD linux/*.exe && echo "- patched interpreter for linux/*.exe's"
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          overlays = [ ];
+        };
+        gentype-source = pkgs.mkYarnPackage {
+          name = "gentype_source";
+          src = ./.;
+          packageJSON = ./package.json;
+          yarnLock = ./yarn.lock;
+          pkgConfig = {
+            rescript = {
+              buildInputs = with pkgs; [ which gcc_multi ];
+              postInstall = ''
+                echo "PATCHELF'ING RESCRIPT EXECUTABLES (INCL NINJA)"
+                # Patching interpreter for linux/*.exe's
+                THE_LD=$(patchelf --print-interpreter $(which mkdir))
+                patchelf --set-interpreter $THE_LD linux/*.exe && echo "- patched interpreter for linux/*.exe's"
 
-              # Replacing needed shared library for linux/ninja.exe
-              THE_SO=$(find /nix/store/*/lib64 -name libstdc++.so.6 | head -n 1)
-              patchelf --replace-needed libstdc++.so.6 $THE_SO linux/ninja.exe && echo "- replaced needed for linux/ninja.exe"
-            '';
+                # Replacing needed shared library for linux/ninja.exe
+                THE_SO=$(find /nix/store/*/lib64 -name libstdc++.so.6 | head -n 1)
+                patchelf --replace-needed libstdc++.so.6 $THE_SO linux/ninja.exe && echo "- replaced needed for linux/ninja.exe"
+              '';
+            };
           };
         };
-      };
-      gentype = pkgs.stdenv.mkDerivation {
-        name = "gentype";
-        src = gentype-source + "/libexec/gentype/deps/gentype";
-        buildInputs = with pkgs; [ ocaml dune_2 ];
-        buildPhase = "dune build";
-        installPhase = ''
-          mkdir -p $out
-          cp -r _build/default/src/GenType.exe $out
-        '';
-      };
-    in {
+        gentype = pkgs.stdenv.mkDerivation {
+          name = "gentype";
+          src = gentype-source + "/libexec/gentype/deps/gentype";
+          buildInputs = with pkgs; [ ocaml dune_2 ];
+          buildPhase = "dune build";
+          installPhase = ''
+            mkdir -p $out
+            cp -r _build/default/src/GenType.exe $out
+          '';
+        };
+      in {
 
-      packages."${system}" = {
-        gentype = gentype;
-      };
-      defaultPackage."${system}" = self.packages."${system}".gentype;
-  };
+        packages = flake-utils.lib.flattenTree { default = gentype; };
+      });
 }
